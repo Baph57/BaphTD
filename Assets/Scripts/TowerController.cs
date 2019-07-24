8ae087326c4207b1dd6533e504e7cd94c1d4e15c
Baph <hybridcrayon@yahoo.com>
2019-07-24 09:03:56 -0700

    Boy, wish the previous commits were attributed to me.. Thanks GitHub Desktop

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //LookAt is a Unity Method, obviously helps a lot
+        objectToPan.LookAt(targetEnemy);
+    }
+}
