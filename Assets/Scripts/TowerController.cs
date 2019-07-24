873366b5c3907419b9b466401c7cb8132c27d53e
Baph <hybridcrayon@yahoo.com>
2019-07-24 14:19:35 -0700

    Towers refactored and implemented with additional logic

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -6,11 +6,55 @@ public class TowerController : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 40f;
+    [SerializeField] ParticleSystem ballisticsParticles;
+
 
     // Update is called once per frame
     void Update()
     {
-        //LookAt is a Unity Method, obviously helps a lot
-        objectToPan.LookAt(targetEnemy);
+        BallisticsController();
+    }
+
+    private void BallisticsController()
+    {
+        //reference to the ballistics particles, .emission is needed to stop the emission
+        var emissionReference = ballisticsParticles.emission;
+
+        //if we have a reference to the target enemy...
+        if (targetEnemy)
+        {
+            //evaluate distance from rotating turret head to target enemy, store in variable
+            float distanceFromEnemyTarget =
+                Vector3.Distance(targetEnemy.transform.position, objectToPan.transform.position);
+
+            //if we are in range
+            if (distanceFromEnemyTarget <= attackRange)
+            {
+                //LookAt is a Unity Method, obviously helps a lot
+                objectToPan.LookAt(targetEnemy);
+                //our ballisticsFX.emission system gets turned on
+                emissionReference.enabled = true;
+            }
+            //out of range
+            else if (distanceFromEnemyTarget > attackRange)
+            {
+                print("enemy out of range");
+                //ballisticsFX turned off
+                emissionReference.enabled = false;
+            }
+            else
+            {
+                //hacky catch
+                print("Shouldn't get here TowerController.cs");
+            }
+
+        }
+        else
+        {
+            //final evaluation to ensure the turret evaluates to off with no enemy
+            emissionReference.enabled = false;
+            print("no enemy");
+        }
     }
 }
